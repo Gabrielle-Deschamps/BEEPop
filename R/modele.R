4bd77ca6017d995c8009940159e69cde3fa860d4
Gabrielle Deschamps <gabrielle.deschamps@orange.fr>
2020-11-11 11:37:05 +0100

    suppression de modele pour pouvoir le remettre

--- R/modele.R
+++ R/modele.R
@@ -1,13 +1,13 @@
-#' Obtenir la taille de la population à l'année suivante dans une communauté
+#' Obtenir la taille des populations à l'année suivante dans une communauté
 #'
 #' @param community
-#' @param K
+#' @param K = capacité biotique du milieu
 #'
-#' @return
+#' @return la communauté avec une colonne en plus pour la taille de chaque population à l'année a+1
 #' @export
 #'
 #' @examples
-modele <- function(community, K){
+model <- function(community, K){
   interaction<-attributes(community)$interaction
   popt0<-as.integer(current_size(community))
   popevol=c()
@@ -22,3 +22,21 @@ modele <- function(community, K){
   community<-append_size(community, as.integer(popevol))
   community
 }
+
+#' Obtenir la taille des populations dans une communauté après x années
+#'
+#' @param community
+#' @param K = capacité biotique du milieu
+#' @param x = nombre d'année qu'on veut prédire
+#'
+#' @return la communauté avec x colonnes en plus pour la taille de chaque population à l'année a+x
+#' @export
+#'
+#' @examples
+model.multi.years <- function(community, K, x){
+  for (i in 1:x){
+    community<-model(community,K)
+  }
+  community
+}
+
